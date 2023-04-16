0d2ae571c5b6d3a4116b248b1cd2e62647e8b02d
SrDiamond <yassin.alaouielaissi@gmail.com>
2023-04-16 20:44:51 +0200

    Ejercicios

---
+++ JavaScript/2.Sintaxis, variables y palabras reservadas de JS/Ejercicio_2.js
@@ -0,0 +1,14 @@
+const persona = {
+    nombre: "Yassin",
+    edad: 20,
+    isDeveloper: true,
+    dateBirth: new Date(2002, 7, 4),
+    libro: {
+        titulo: "Reinos de Runaterra",
+        autor: "Riot Games Merchandise Inc.",
+        fecha: new Date(2020, 9, 8),
+        url: "https://www.amazon.es/League-Legends-Runaterra-ficción-ilustrados/dp/8418318198"
+    }
+}
+
+console.log(persona)
