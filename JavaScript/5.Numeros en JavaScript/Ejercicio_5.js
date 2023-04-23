a53fc3773457244e295b758f5d5393d1fce2db0b
SrDiamond <yassin.alaouielaissi@gmail.com>
2023-04-23 15:48:39 +0200

    Ejercicio

--- JavaScript/5.Numeros en JavaScript/Ejercicio_5.js
+++ JavaScript/5.Numeros en JavaScript/Ejercicio_5.js
@@ -1,6 +1,8 @@
 let altura = 174;
-let alutra_f = 1.74;
+let altura_f = 1.74;
 let peso_f = 50;
-let altura_r = Math.round(1.74);
+
+let altura_r = Math.round(altura_f);
+let peso_r = Math.floor(peso_f)
 let max_numero = Number.MAX_VALUE + 1;
 console.log(max_numero === Number.MAX_VALUE)
